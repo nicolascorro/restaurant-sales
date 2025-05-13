324e040aef35180f981f81b219b3ebbe3d67622b
nicolas-labe <nicolascorro@labeabogados.com>
2025-05-13 11:31:11 +0200

    allow deployment of render

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Restaurant Sales Prediction API")
 # Configure CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["https://restaurant-sales.vercel.app/", "http://localhost:3000"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
